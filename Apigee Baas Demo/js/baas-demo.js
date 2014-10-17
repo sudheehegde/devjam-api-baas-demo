99aac4b05e1f5673a88b91587b574104843224f2
sudheendra@apigee.com <sudheendra@apigee.com>
2014-10-17 14:27:23 +0530

    Added HTML pages
    Added HTML pages

---
+++ Apigee Baas Demo/js/baas-demo.js
@@ -0,0 +1,28 @@
+//STEP 2 Configure BaaS Username
+
+//End of STEP 2
+
+// STEP 3 Create Apigee Client 
+
+// STEP 5 Code for Follow/UnFollow toggle.
+//Add STEP 5 Code here 
+//End of STEP 5
+
+
+// STEP 6: Add method to Load all Cities
+// Add step 6 code here
+// End of STEP 6 
+
+
+// STEP 7: Add method to Load Cities being followed
+//Add step 6 code here
+// End of STEP 7 
+
+// STEP 8: Add createRelationship() function 
+// Add createRelationship code here
+// End of STEP 8
+
+
+// STEP 9: Add createActivitySteam() function
+// Add createActivitySteam code here 
+// End STEP 9
